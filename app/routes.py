e9982c440cf261b5f0fddc4d241d7e79680d23d7
RCJK33 <rafa.11.rcj@gmail.com>
2023-06-26 00:21:15 -0700

    Class 1 FSDI 111, Route about me, returning a dictionary

--- app/routes.py
+++ app/routes.py
@@ -3,27 +3,33 @@ from flask import Flask, request
 app = Flask(__name__)
 USERS = []
 
-@app.get("/users/<int:user_id>")
 @app.get("/")
-def index(user_id):
+def index():
+    return {'message': 'Hello World'}
+
+@app.get("/aboutme")
+def about_me():
+    return get_users_by_id(1)
+
+@app.get("/users")
+def get_users():
+    return {'users': USERS}
+
+@app.get("/users/<int:user_id>")
+def get_users_by_id(user_id):
     me = {}
     not_found = True
     if user_id == 1:
         me =  {
             'first_name': 'Rafael',
             'last_name': 'Chavez',
-            'hobbies': ['games', 'programming', 'movies'],
-            'is_programmer': True,
-            'age': 19,
+            'hobbies': 'games, programming and movies',
+            'is_active': True,
         }
         not_found = False
     if not_found:
         return me, 404
     return me
-
-@app.get("/users")
-def get_users():
-    return {'users': USERS}
 
 @app.post("/users")
 def create_user():
